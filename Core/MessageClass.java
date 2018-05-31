8e98e9871aba5620b2ffe7511718f5eeb2d3b71f
rollingflamingo <andrea.storace.uni@gmail.com>
2018-05-31 12:35:51 +0200

    uploaded files from remote

---
+++ Core/MessageClass.java
@@ -0,0 +1,23 @@
+import java.io.Serializable;
+
+public class MessageClass implements Serializable {
+    private String Text;
+    private String User;
+
+    public MessageClass(String user, String text){
+        Text = text;
+        User = user;
+    }
+
+    public String getText(){
+        return Text;
+    }
+
+    public String getUser(){
+        return User;
+    }
+
+    public String getFormatMsg(){
+        return  "[" + getUser()+"]: "+getText();
+    }
+}
---
+++ Core/RMIClient.java
@@ -0,0 +1,6 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface RMIClient extends Remote {
+    void CLiNotify(String TopicLabel, String TriggeredBy, boolean type) throws RemoteException;
+}
