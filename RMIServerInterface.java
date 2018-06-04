369a5b83bb8f234e276afce9922a84f0bbdc34da
rollingflamingo <andrea.storace.uni@gmail.com>
2018-06-04 16:58:14 +0200

    added notifyHandler class, modified notify() function

--- RMIServerInterface.java
+++ RMIServerInterface.java
@@ -1,7 +1,7 @@
 package RMICore;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 /* sentiti libero di fare le modifiche che vuoi.. ho solo messo per ora dei parametri che mi venivano bene nel lato client*/
 public interface RMIServerInterface extends Remote {
@@ -9,5 +9,5 @@ public interface RMIServerInterface extends Remote {
     boolean ManageSubscribe(String TopicLabel, String User, boolean unsubscribe) throws RemoteException; /* metodo per iscrizione a topic... manca la dichiarazione di un metdodo per iscrizione al forum stesso*/
     boolean ManagePublish(MessageClass msg, String TopicName) throws RemoteException; // add message to  a topic convo
     boolean ManageAddTopic(String TopicName, String TopicOwner) throws RemoteException;
-    HashMap<String, TopicClass> getTopics() throws RemoteException;
+    ConcurrentHashMap<String, TopicClass> getTopics() throws RemoteException;
 }
