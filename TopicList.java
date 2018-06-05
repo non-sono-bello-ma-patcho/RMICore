3ea8d27058d9992ac0a3e31a00c813ed5bfa7b70
rollingflamingo <andrea.storace.uni@gmail.com>
2018-06-05 17:35:07 +0200

    fixed minor issues..

--- TopicList.java
+++ TopicList.java
@@ -3,6 +3,7 @@ package RMICore;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class TopicList {
     private CopyOnWriteArrayList<TopicClass> topicList;
@@ -42,7 +43,17 @@ public class TopicList {
         return null;
     }
 
-    public T put(TopicClass tc){
+    public Boolean put(TopicClass tc){
         return topicList.add(tc);
     }
+
+    public Boolean remove(String TopicName){
+        for(TopicClass t : topicList)
+            if(t.getName.equals(TopicName)){
+                topicList.remove(t);
+                return true;
+            }
+        return false;
+    }
+
 }
