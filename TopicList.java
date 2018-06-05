2e79d19f445a899bcad74a35bcb593e2cf164cb6
rollingflamingo <andrea.storace.uni@gmail.com>
2018-06-05 17:08:49 +0200

    added topiclist to improve information hiding...

---
+++ TopicList.java
@@ -0,0 +1,44 @@
+package RMICore;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+public class TopicList {
+    private List<TopicClass> topicList;
+
+    public TopicList(){
+        topicList = new ArrayList<>();
+    }
+
+    /**
+     *
+     * @return list of registered topics.
+     */
+    public List<String> ListTopicName(){
+        List<String> res = new ArrayList<>();
+        for (TopicClass t : topicList){
+            res.add(t.getName());
+        }
+        return res;
+    }
+
+    /**
+     *
+     * @param topic: named of the searched topic
+     * @return
+     */
+    public boolean contains(String topic){
+        return ListTopicName().contains(topic);
+    }
+
+    public List<MessageClass> getConversation(String topicname){
+        return getTopicNamed(topicname).getMessagesAsMessage();
+    }
+
+    private TopicClass getTopicNamed(String topicName) throws NoSuchElementException{
+        if(!contains(topicName)) throw new NoSuchElementException();
+        for(TopicClass t : topicList) if(t.getName().equals(topicName)) return t;
+        return null;
+    }
+}
