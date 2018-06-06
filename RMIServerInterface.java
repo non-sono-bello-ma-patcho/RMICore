b8e9e6da802c1f479f1f96bc92dc9acbe1083b43
Andrea Storace <andrea.storace.uni@gmail.com>
2018-06-06 18:47:29 +0200

    added connection response enum

--- RMIServerInterface.java
+++ RMIServerInterface.java
@@ -5,7 +5,8 @@ import java.util.HashMap;
 
 /* sentiti libero di fare le modifiche che vuoi.. ho solo messo per ora dei parametri che mi venivano bene nel lato client*/
 public interface RMIServerInterface extends Remote {
-    boolean ManageConnection(String username, String password, String address, String op) throws RemoteException;
+    enum ConnResponse { UnsetPolicy, NoSuchObject, NothingToExport };
+    ConnResponse ManageConnection(String username, String password, String address, String op) throws RemoteException;
     boolean ManageSubscribe(String TopicLabel, String User, boolean unsubscribe) throws RemoteException; /* metodo per iscrizione a topic... manca la dichiarazione di un metdodo per iscrizione al forum stesso*/
     boolean ManagePublish(MessageClass msg, String TopicName) throws RemoteException; // add message to  a topic convo
     boolean ManageAddTopic(String TopicName, String TopicOwner) throws RemoteException;
