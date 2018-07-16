37d41486e12cd87b42a56619cb67e6473c6a9edb
rollingflamingo <andrea.storace.uni@gmail.com>
2018-07-16 14:45:50 +0200

    modified interface

--- RMIServerInterface.java
+++ RMIServerInterface.java
@@ -1,14 +1,16 @@
 package RMIForum.RMICore;
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-import java.util.concurrent.ConcurrentHashMap;
+        import java.rmi.Remote;
+        import java.rmi.RemoteException;
+        import java.util.List;
+        import java.util.concurrent.ConcurrentHashMap;
 
 /* sentiti libero di fare le modifiche che vuoi.. ho solo messo per ora dei parametri che mi venivano bene nel lato client*/
 public interface RMIServerInterface extends Remote {
-    enum ConnResponse { UnsetPolicy, NoSuchObject, AlreadyExist, Success, NoSuchUser };
-    ConnResponse ManageConnection(String username, RMIClient stub, String op) throws RemoteException;
-    boolean ManageSubscribe(String TopicLabel, String User, boolean unsubscribe) throws RemoteException; /* metodo per iscrizione a topic... manca la dichiarazione di un metdodo per iscrizione al forum stesso*/
-    boolean ManagePublish(MessageClass msg, String TopicName) throws RemoteException; // add message to  a topic convo
-    boolean ManageAddTopic(String TopicName, String TopicOwner) throws RemoteException;
-    TopicList getTopics() throws RemoteException;
+        enum ConnResponse { UnsetPolicy, NoSuchObject, AlreadyExist, Success, NoSuchUser, cyclicityDetected};
+        ConnResponse ManageConnection(String username, RMIClient stub, List<String> BrokerID, String op) throws RemoteException;
+        boolean ManageSubscribe(String TopicLabel, String User, boolean unsubscribe) throws RemoteException; // metodo per iscrizione a topic... manca la dichiarazione di un metdodo per iscrizione al forum stesso*/
+        boolean ManagePublish(MessageClass msg, String TopicName) throws RemoteException; // add message to  a topic convo
+        boolean ManageAddTopic(String TopicName, String TopicOwner) throws RemoteException;
+        boolean ImConnected(String user);
+        TopicList getTopics() throws RemoteException;
 }
